c991f77fcb583f2fd6d4da608b0230f395c74fa8
yinhang_sun <sunyinhang@gmail.com>
2023-01-30 08:40:38 -0800

    Exploring Spring Security - Storing Bcrypt Encoded Passwords

--- src/main/java/com/yinhang/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/yinhang/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -71,12 +71,16 @@ public class BasicAuthSecurityConfiguration {
 	public UserDetailsService userDetailsService(DataSource dataSource) {
 		
 		var user = User.withUsername("Yinhang")
-			.password("{noop}dummy")
+			//.password("{noop}dummy")
+			.password("dummy")
+			.passwordEncoder(str -> passwordEncoder().encode(str))
 			.roles("USER")
 			.build();
 		
 		var admin = User.withUsername("admin")
-				.password("{noop}dummy")
+				//.password("{noop}dummy")
+				.password("dummy")
+				.passwordEncoder(str -> passwordEncoder().encode(str))
 				.roles("ADMIN", "USER")
 				.build();
 		
@@ -84,6 +88,11 @@ public class BasicAuthSecurityConfiguration {
 		jdbcUserDetailsManager.createUser(user);
 		jdbcUserDetailsManager.createUser(admin);
 		return jdbcUserDetailsManager;
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 	
